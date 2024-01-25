3a6c9e4acc886ad5752b6fd186ceb64ff38087ef
nathinsubramani <snathin0303@gmail.com>
2024-01-25 22:44:26 +0530

    version_2

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Mobile from './Mobile'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <Mobile/>
+    </div>
   )
 }
---
+++ src/Pages/Mobile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Mobile.css";
+export default function Mobile() {
+  return (
+    <div className='sample'>
+    <div className='new'> <a href="tel:+919597797732">CLICK TO CALL</a><br></br>
+        <a href="mailto:snathin0303@gmail.com"> CLICK HERE TO MAIL </a></div>
+    </div>
+  )
+}
